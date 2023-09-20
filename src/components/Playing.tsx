01d9c78bd27150b8458b84c70de8b75f45950fae
Vana <vle66770@gmail.com>
2023-09-20 21:49:19 +0700

    Mô tả các thay đổi của bạn ở đây

--- src/components/Playing.tsx
+++ src/components/Playing.tsx
@@ -4,22 +4,23 @@ import "react-h5-audio-player/lib/styles.css";
 import { SongType } from "./Context";
 
 interface IProps {
-    song: SongType
-    handleSetSong: (id: number) => void
+  song: SongType
+  handleSetSong: (id: number) => void
 }
 
 const Playing: React.FC<IProps> = (props) => {
-    const {song, handleSetSong} = props
+  const { song, handleSetSong } = props
   return (
     <div>
       <AudioPlayer
         className="player-music"
         src={song.url}
+        onEnded={() => { handleSetSong(song.id + 1) }}
         layout="stacked-reverse"
         showSkipControls={true}
         showJumpControls={false}
-        onClickNext={() => {handleSetSong(song.id + 1)}}
-        onClickPrevious={() => {handleSetSong(song.id - 1)}}
+        onClickNext={() => { handleSetSong(song.id + 1) }}
+        onClickPrevious={() => { handleSetSong(song.id - 1) }}
       />
     </div>
   );
